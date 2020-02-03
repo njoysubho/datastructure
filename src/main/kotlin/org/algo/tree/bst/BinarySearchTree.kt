1b5fea96e3746ce979b2ed13f47880e4af8b5c27
sabyasachi <njoysubho@gmail.com>
2020-02-03 07:14:56 +0100

    BST delete

--- src/main/kotlin/org/algo/tree/bst/BinarySearchTree.kt
+++ src/main/kotlin/org/algo/tree/bst/BinarySearchTree.kt
@@ -27,12 +27,44 @@ class BinarySearchTree<T : Comparable<T>, V : Comparable<V>> {
         return node
     }
 
-    fun min(x:Node<T,V>):Node<T,V>{
-        if(x.left==null) {
+    fun min(x:Node<T,V>?):Node<T,V>?{
+        if(x?.left==null) {
             return x
         }else{
             return min(x.left!!)
         }
 
+    }
+
+    fun deleteMin(){
+        root=deleteMin(root)
+    }
+
+    private fun deleteMin(x:Node<T,V>?):Node<T,V>?{
+        if(x?.left==null) return x?.right
+        x.left=deleteMin(x.left)
+        return x
+    }
+
+    fun delete(key:T){
+        root=delete(root,key)
+    }
+    private fun delete(x:Node<T,V>?,key:T):Node<T,V>?{
+        var _x = x
+        if(x==null) return null
+        if(key.compareTo(x.key)<0) {
+            return delete(x.left,key)
+        }
+        else if(key.compareTo(x.key)>0){
+            return delete(x.right,key)
+        }else {
+            if(x?.right==null) return x?.left
+            if(x?.left==null) return x?.right
+            var t = _x
+            _x = min(t?.right)
+            x?.right = deleteMin(t?.right)
+            x?.left = t?.left
+        }
+        return x
     }
 }
